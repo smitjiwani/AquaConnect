819e8fd234ed8a76c753d57abb233e487e942a83
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-01-18 01:43:45 +0530

    chore: added seed data

--- server/db/seeds/seed_file.js
+++ server/db/seeds/seed_file.js
@@ -6,27 +6,111 @@ const userID = uuidv4()
 
 const seed = async () => {
   try {
-    // Insert gp_head table
+    // Insert users table
     await db('users').insert({
       user_id: userID,
       village_name: 'dhanushkodi',
-      village_address: 'bihar',
+      village_address: 'sangli',
       contact_no: '1234567890',
-      email_id: 'gphead@gmail.com',
-      password: 'password1',
-      role: 'GP',
+      email_id: 'user1@gmail.com',
+      password: '1',
+      role: 'user',
       member_name: 'biharilal',
     })
-    console.log('Added sample gp head')
+    await db('users').insert({
+      user_id: userID,
+      village_name: 'bimbal',
+      village_address: 'nashik',
+      contact_no: '1234567890',
+      email_id: 'user2@gmail.com',
+      password: '2',
+      role: 'user',
+      member_name: 'shambhu',
+    })
+    await db('users').insert({
+      user_id: userID,
+      village_name: 'ayugya',
+      village_address: 'pune',
+      contact_no: '1234567890',
+      email_id: 'user3@gmail.com',
+      password: '3',
+      role: 'user',
+      member_name: 'chintamani',
+    })
+    await db('users').insert({
+      user_id: userID,
+      village_name: 'pinchkoli',
+      village_address: 'solapur',
+      contact_no: '1234567890',
+      email_id: 'user4@gmail.com',
+      password: '4',
+      role: 'user',
+      member_name: 'chetan',
+    })
+    await db('users').insert({
+      user_id: userID,
+      village_name: 'sitarampur',
+      village_address: 'aurangabad',
+      contact_no: '1234567890',
+      email_id: 'user5@gmail.com',
+      password: '5',
+      role: 'user',
+      member_name: 'banke',
+    })
+    console.log('Added sample users ')
 
     // Insert into ngo table
     await db('ngos').insert({
       ngo_id: ngoID,
       ngo_name: 'aga khan foundation',
       ngo_type: 'jal jeevan mission',
-      ngo_address: 'bihar',
-      email_id: 'ngo@gmail.com',
-      password: 'password1',
+      ngo_address: 'sangli',
+      email_id: 'ngo1@gmail.com',
+      password: '1',
+      role: 'NGO',
+      member_name: 'shambhulal',
+      contact_no: '1234567890',
+    })
+    await db('ngos').insert({
+      ngo_id: ngoID,
+      ngo_name: 'shambhaji trust',
+      ngo_type: 'jal jeevan mission',
+      ngo_address: 'sangli',
+      email_id: 'ngo2@gmail.com',
+      password: '2',
+      role: 'NGO',
+      member_name: 'shambhulal',
+      contact_no: '1234567890',
+    })
+    await db('ngos').insert({
+      ngo_id: ngoID,
+      ngo_name: 'paani jeevan foundation',
+      ngo_type: 'jal jeevan mission',
+      ngo_address: 'sangli',
+      email_id: 'ngo3@gmail.com',
+      password: '3',
+      role: 'NGO',
+      member_name: 'shambhulal',
+      contact_no: '1234567890',
+    })
+    await db('ngos').insert({
+      ngo_id: ngoID,
+      ngo_name: 'nanhe paani foundation',
+      ngo_type: 'jal jeevan mission',
+      ngo_address: 'nashik',
+      email_id: 'ngo4@gmail.com',
+      password: '4',
+      role: 'NGO',
+      member_name: 'shambhulal',
+      contact_no: '1234567890',
+    })
+    await db('ngos').insert({
+      ngo_id: ngoID,
+      ngo_name: 'chote haath foundation',
+      ngo_type: 'jal jeevan mission',
+      ngo_address: 'nashik',
+      email_id: 'ngo5@gmail.com',
+      password: '5',
       role: 'NGO',
       member_name: 'shambhulal',
       contact_no: '1234567890',
