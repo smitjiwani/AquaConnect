b844ab560bc762f0a471db91886107f45f4cbae7
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-01-18 11:46:26 +0530

    Create user bag of words
    Created a function to convert the user query in a bag of words for
    feeding it to the neural network and also created a chat function for
    the user to talk to the bot and get appropriate responses.

--- server/chatbot.py
+++ server/chatbot.py
@@ -28,3 +28,39 @@ def develop_model(input_size,output_size):
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
+
+# Define a function to convert a user query into a bag of words
+def bag_of_words(query, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(query)
+    s_words = [lematizer.lemmatize(word.lower()) for word in s_words if word != '?']
+
+    for i in s_words:
+        if i in words:
+            bag[words.index(i)] = 1
+
+    return bag  
+
+# Define the chat function
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+        if use_pre_trained_model and pre_trained_model:
+            results = pre_trained_model.predict([bag_of_words(inp, words)])[0]
+        else:    
+            results = model.predict([bag_of_words(inp, words)])[0]
+
+        results_index = np.argmax(results)
+        tag = labels[results_index]
+        
+        if results[results_index] > 0.7:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+                    print(random.choice(responses))
+        else:
+            print("I apologise but I can't quite get you, could you please repeat your query")  
