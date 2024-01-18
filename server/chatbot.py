9952821161203971c3a477dccb09f960f27d77cc
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-01-18 12:30:00 +0530

    Pre process data and train model
    Created the required bag of words out of all the data available in intent.json file and trained the model on the basis of it, also added features like error handling and using the saved model etc.

--- server/chatbot.py
+++ server/chatbot.py
@@ -64,3 +64,81 @@ def chat():
                     print(random.choice(responses))
         else:
             print("I apologise but I can't quite get you, could you please repeat your query")  
+
+# Initialize variables for pre-trained model and flag for using it
+pre_trained_model = None
+use_pre_trained_model = False
+
+# Attempt to load pre-processing data and pre-trained model
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+
+    try:
+        pre_trained_model = load_model('model_keras.h5')
+        use_pre_trained_model = True
+
+    except FileNotFoundError:
+        pass            
+
+except FileNotFoundError:
+    # If pre-processing data or pre-trained model not found, perform data processing and training
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [lematizer.lemmatize(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [lematizer.lemmatize(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = np.array(training)
+    output = np.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+    input_size = len(training[0])
+    output_size = len(output[0])
+
+    # Create and train the neural network model
+    model = develop_model(input_size,output_size)
+    model.fit(training, output, epochs=1000, batch_size=8, verbose=1) 
+    model.save("model_keras.h5")
+
+# Entry point of the script
+if __name__=='__main__':
+    chat()
